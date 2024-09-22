f295e03e45c12b3d0aa41de22d9a2715b0742630
Konstantin <konst.kazan@gmail.com>
2024-09-22 21:23:47 +0300

    2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    $tags = ['обучение', 'программирование', 'php', 'oop'];
+    return view('about', ['tags' => $tags]);
+});
