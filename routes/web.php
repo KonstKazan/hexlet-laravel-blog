b183d90a2159345ea024266b90e445313481a475
Konstantin <konst.kazan@gmail.com>
2024-09-27 19:38:37 +0300

    add update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ Route::get('about', [PageController::class, 'about'])
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
 
-    Route::get('articles/{id}', [ArticleController::class, 'show'])
+Route::get('articles/create', [ArticleController::class, 'create'])
+    ->name('articles.create');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
     ->name('articles.show');
+
+Route::post('articles', [ArticleController::class, 'store'])
+    ->name('articles.store');
+
+Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
+    ->name('articles.edit');
+
+Route::patch('articles/{id}', [ArticleController::class, 'update'])
+    ->name('articles.update');
