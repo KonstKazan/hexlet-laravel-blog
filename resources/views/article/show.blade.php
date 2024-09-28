beb0ce1723a0c1b063838a6015b38c8971179c5e
Konstantin <konst.kazan@gmail.com>
2024-09-28 13:47:18 +0300

    add delete

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <h1>{{$article->name}}</h1>
     <div>{{$article->body}}</div>
+    <a href="articles/{{$article->id}}" data-method="delete" rel="nofollow">Удалить</a>
 @endsection
